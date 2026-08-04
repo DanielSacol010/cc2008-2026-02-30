b1d119697a7ca54bb3b6913df54edec57a246eeb
DanielSacol010 <danieledusc@gmail.com>
2026-08-03 20:20:43 -0600

    se agrega clase Ticket

---
+++ FIFA/src/Ticket.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+
+public class Ticket {
+    private int numero;
+
+    // El constructor ya no recibe parámetros, se autogenera el número.
+    public Ticket() {
+        Random rand = new Random();
+        this.numero = rand.nextInt(15000) + 1; // Genera de 1 a 15,000
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public boolean esElegibleParaCompra() {
+        Random rand = new Random();
+        int a = rand.nextInt(15000) + 1;
+        int b = rand.nextInt(15000) + 1;
+        
+        int limiteInferior = Math.min(a, b);
+        int limiteSuperior = Math.max(a, b);
+        
+        return numero >= limiteInferior && numero <= limiteSuperior;
+    }
+}
